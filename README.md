9383602be7f04fb27146c1b5a00aea65a6618c54
Toshiyuki Suzumura <suz@smoche.info>
2022-12-20 16:31:37 +0900

    QRコード出力を実装

---
+++ README.md
@@ -0,0 +1,9 @@
+crypto利用のお試し
+====
+
+`{ id: 99999999, location: 'Asia/Tokyo' }` ～ `{ id: 100000008, location: 'Asia/Tokyo' }` を格納した
+`https://example.com/<暗号化文字列>` を生成して `tmp/*.png` にそのQRコードを出力します。
+
+```bash
+$ yarn start
+```
--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import qrcode from 'qrcode';
 import KujiCrypto from "./KujiCrypto";
 
+const baseUrl = 'https://example.com/';
 const KujiGen = new KujiCrypto('p@ssWorD', 'ThE-sAlT', '9510ed5c321945212a57adc857bb42d8');
 
 for (let did = 0; did < 10; ++did) {
   const encryptedId = KujiGen.encrypt({id: 9999_9999 + did, location: 'Asia/Tokyo'});
-  const url = `https://example.com/${encryptedId}`;
+  const url = `${baseUrl}${encryptedId}`;
   const decrypted = KujiGen.decrypt(encryptedId);
   console.log(url, decrypted);
+  qrcode.toFile(`tmp/${did}.png`, url);
 }
